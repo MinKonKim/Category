c3143c4e9afa6cabf0e11d6568a0eeef3934964d
Minkon Kim <alsrhs98@gmail.com>
2024-03-19 14:32:07 +0900

    Update Server

--- qapage/src/adminpage/AdminPage.js
+++ qapage/src/adminpage/AdminPage.js
@@ -1,8 +1,14 @@
 import React from "react";
+import axios from "axios";
+// axios를 쓰는 목적에 서버에 데이터를 요청할때,
+// 비동기적으로 요청 하려고
 
 export const AdminPage = () => {
-  const commitAll = () => {
+  const commitAll = async () => {
     alert("commitAll");
+    //    값을 다 받아올때 까지 기다린다 : await
+    const result = await axios.get("/admin");
+    console.log(result);
   };
 
   return (
--- qapage/src/qapage/QaPage.js
+++ qapage/src/qapage/QaPage.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import axios from "axios";
 export const QaPage = () => {
+  const selectWhere = () => {
+    alert("selectWhere!");
+    axios.get("/qapage/" + 3);
+  };
+
   return (
     <div>
       <h1>QA 화면입니다.</h1>
