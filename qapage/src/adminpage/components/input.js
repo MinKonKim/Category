8fc410ce82902175a69fbbca3ffff29c794d8e9e
Minkon Kim <alsrhs98@gmail.com>
2024-03-20 15:32:06 +0900

    init

--- qapage/src/adminpage/components/input.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const input = () => {
-  return <div>input</div>;
-};
--- qapage/src/api/config/db.js
+++
@@ -1,8 +0,0 @@
-var mysql = require("mysql");
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "blog",
-});
-module.exports = db;
